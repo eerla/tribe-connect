3464fa3c3158c1d56f3bac9c7215163f2cfc876f
eerla <gurubrahmam443@gmail.com>
2026-01-12 21:33:38 -0500

    Update 001_init.sql

--- src/sql/001_init.sql
+++ src/sql/001_init.sql
@@ -602,3 +602,7 @@ BEGIN
   RAISE NOTICE '  - Slug history for 301 redirects';
   RAISE NOTICE '  - is_cancelled, is_deleted columns added';
 END $$;
+
+
+-- Add is_new_user column to profiles table
+ALTER TABLE profiles ADD COLUMN IF NOT EXISTS is_new_user BOOLEAN DEFAULT true;
